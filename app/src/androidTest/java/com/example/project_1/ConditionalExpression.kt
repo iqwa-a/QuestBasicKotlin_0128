38714193ad9c091fefdda42c9596fcc37a4da9c3
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-10-12 23:49:04 +0700

    membuat fun withoutParameter dan named argu serta membetulkan eror di ConditionalExpression

--- app/src/androidTest/java/com/example/project_1/ConditionalExpression.kt
+++ app/src/androidTest/java/com/example/project_1/ConditionalExpression.kt
@@ -66,4 +66,3 @@ fun main() {
     ConditionalStatment()
     Perulangan()
 }
-}
---
+++ app/src/androidTest/java/com/example/project_1/Functions.kt
@@ -0,0 +1,21 @@
+package com.example.project_1
+
+fun withoutParameter() {
+    println(" == withoutParameter == ")
+    println("Hello, World!")
+}
+
+    fun withParameter(name: String) {
+        println()
+        println(" == withParameter == ")
+        println("Hello, $name!")
+    }
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println(" == withNamedArgument == ")
+    println("Hello, $name! You are $age years old.")
+}
+
