d88d378a17b1eb315f04e18f62c121ff9f7598e5
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-10-12 23:32:00 +0700

    Membuat Fun perulangan

--- app/src/androidTest/java/com/example/project_1/ConditionalExpression.kt
+++ app/src/androidTest/java/com/example/project_1/ConditionalExpression.kt
@@ -26,4 +26,44 @@ fun ConditionalStatment() {
         7 -> println("Saturday")
         else -> println("Invalid Day")
     }
+    // Try-Catch
+    val value = "10"
+    try{
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException){
+        println("Invalid Number")
+    }
+}
+
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("Bmw", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1
+    do{
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
+
+fun main() {
+    ConditionalStatment()
+    Perulangan()
+}
 }
