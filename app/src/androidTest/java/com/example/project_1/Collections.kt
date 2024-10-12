17033cb27b7128e2a769d9fca07cabf333558ffe
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-10-12 22:54:25 +0700

    Membuat map Mutable

--- app/src/androidTest/java/com/example/project_1/Collections.kt
+++ app/src/androidTest/java/com/example/project_1/Collections.kt
@@ -45,5 +45,10 @@ fun ContohMap() {
 // Map Read-Only
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
 }
+
 
