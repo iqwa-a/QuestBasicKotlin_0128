f2ae333fdf92105749dcedf3f7c1341bb5623e50
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-10-12 23:29:04 +0700

    Membuat If Else dan When

---
+++ app/src/androidTest/java/com/example/project_1/ConditionalExpression.kt
@@ -0,0 +1,29 @@
+package com.example.project_1
+
+/// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expresion menggunakan if-else, when, dan try-catch
+fun ConditionalStatment() {
+    println("=== Conditional Expression")
+
+// If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+// When
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
+}
