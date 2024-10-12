774b3f8b336953d76cc5b868b8bb91ab2d8b48e2
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-10-12 22:49:07 +0700

    Membuat List Mutable

--- app/src/androidTest/java/com/example/project_1/Collections.kt
+++ app/src/androidTest/java/com/example/project_1/Collections.kt
@@ -11,6 +11,25 @@ fun ContohList() {
 // List Read-Only
 val readOnlyAbjad = listOf ("A", "B", "C" )
     println(readOnlyAbjad)
+//List Mutable
+ val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+ println(shape)
+
+    // Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Mutable
+    shape. remove("Triangle")
+    println(shape)
+
+    // Mengubah data di dalam List Mutable
+    shape [0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
 
 }
 
