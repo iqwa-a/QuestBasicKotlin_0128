745be24726cd72450c05fe5fe23b442ac36f4e29
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2024-10-12 22:26:11 +0700

    Membuat Kumpulan data yang disusun secara berurutan yang dan bersifat orderan

---
+++ app/src/androidTest/java/com/example/project_1/Collections.kt
@@ -0,0 +1,14 @@
+package com.example.project_1
+
+// List adalah Kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan list0f
+// List Mutable menggunakan mutableList0f
+
+fun ContohList() {
+    println(" === List === ")
+}
+// List Read-Only
+    val read0nlyAbjad = list0f("A", "B", "C")
+    println(readOnlyAbjad)
